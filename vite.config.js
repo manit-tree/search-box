6b749f3beb6ab6d72ab280dcba60fe92f3ecb741
8columns <manit.tree@gmail.com>
2024-10-12 22:09:54 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import { defineConfig } from 'vite';
+import fs from 'fs';
+
+export default defineConfig({
+    plugins: [cssInjectedByJsPlugin()],
+    css: {
+        transformer: 'lightningcss',
+    },
+    build: {
+        outDir: './dist',
+        minify: false,
+        sourcemap: false,
+        emptyOutDir: true,   
+
+        lib: {
+            entry: './modules/search-box/index.js',
+            name:'SearchBox',
+            formats: ['iife','esm'],
+            fileName: (format) => `[name].[format].js`
+        }
+    },
+    server: {
+        https: {
+            key: fs.readFileSync(`./server.key`),
+            cert: fs.readFileSync(`./server.crt`)
+        },
+        open: './index.html'
+    }
+})
